60065fcdc179276cd9568892f6971e5f89ff886b
Gustas <gustass1@gmail.com>
2021-06-17 12:03:06 +0300

    feat: added about page

---
+++ pages/about.tsx
@@ -0,0 +1,23 @@
+import { Layout } from '../src/features/layout';
+
+export default function About() {
+	return (
+		<Layout>
+			<div className="bg-white h-full">
+				<section>
+					<h1>About</h1>
+					<article>
+						<p>Brandstatement: (Audrey)</p>
+						<p>
+							The core brand values of the innovation lab are: Innovation,
+							connection and open. These 3 pillars are the centre of the
+							institution. And they resonate in all the elements of the
+							identity.
+						</p>
+						<p>The tone of voice (Audrey)</p>
+					</article>
+				</section>
+			</div>
+		</Layout>
+	);
+}
