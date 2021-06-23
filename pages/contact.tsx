590fa5973cb4aeb9376dd34a8decc59108869bdb
Lukas <theluksys3@gmail.com>
2021-06-23 10:21:59 +0200

    fix: detailed fixes

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -5,11 +5,11 @@ export default function Contact() {
 	return (
 		<Layout>
 			<div className="flex bg-white h-full pb-32">
-				<section className="flex flex-col justify-center mb-28 fontys-light w-full max-w-screen-lg mx-auto">
-					<h1 className="text-5xl tracking-widest mt-32 mb-24 text-center">
+				<section className="flex flex-col justify-center mb-28 fontys-light w-full  max-w-sm  mx-auto">
+					<h1 className="text-5xl tracking-widest mt-32 mb-24 text-left">
 						CONTACT
 					</h1>
-					<article className="space-y-16 w-full max-w-sm mx-auto">
+					<article className="space-y-16 w-full mx-auto">
 						<form action="">
 							<div className="space-y-8 mb-16">
 								<input
@@ -18,7 +18,7 @@ export default function Contact() {
 									name="name"
 									className="py-4 px-5 w-full shadow-lg rounded-xl focus:outline-none"
 									style={{
-										boxShadow: '0 7px 23px 1px rgb(0 0 0 / 25%)'
+										boxShadow: '0 7px 16px 1px rgb(0 0 0 / 14%)'
 									}}
 								/>
 								<input
@@ -27,7 +27,7 @@ export default function Contact() {
 									name="email"
 									className="py-4 px-5 w-full shadow-lg rounded-xl focus:outline-none"
 									style={{
-										boxShadow: '0 7px 23px 1px rgb(0 0 0 / 25%)'
+										boxShadow: '0 7px 16px 1px rgb(0 0 0 / 14%)'
 									}}
 								/>
 								<input
@@ -36,7 +36,7 @@ export default function Contact() {
 									placeholder="your company"
 									className="py-4 px-5 w-full shadow-lg rounded-xl focus:outline-none"
 									style={{
-										boxShadow: '0 7px 23px 1px rgb(0 0 0 / 25%)'
+										boxShadow: '0 7px 16px 1px rgb(0 0 0 / 14%)'
 									}}
 								/>
 							</div>
@@ -45,18 +45,16 @@ export default function Contact() {
 								placeholder="message"
 								className="py-4 px-5 mb-8 w-full shadow-lg rounded-xl focus:outline-none resize-none h-36"
 								style={{
-									boxShadow: '0 7px 23px 1px rgb(0 0 0 / 25%)'
+									boxShadow: '0 7px 16px 1px rgb(0 0 0 / 14%)'
 								}}
 							></textarea>
 
-							<div className="flex justify-center">
-								<button
-									type="submit"
-									className="px-12 py-3 rounded-lg bg-black text-white focus:outline-none"
-								>
-									Send
-								</button>
-							</div>
+							<button
+								type="submit"
+								className="px-12 py-3 rounded-lg bg-black text-white focus:outline-none"
+							>
+								Send
+							</button>
 						</form>
 					</article>
 				</section>
